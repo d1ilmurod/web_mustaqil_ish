4a2af384b6df4d420164f7d4b20e6f77fdd7ee84
Dilmurod <142794823+d1ilmurod@users.noreply.github.com>
2024-04-28 15:40:29 +0500

    Admin paneldan yuklangan rasimlari chiqadon bo'ldi

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductAll
+from .views import index_page_view
 
 
 urlpatterns = [
-    path('',ProductAll.as_view(),name='products')
+    # path('',ProductAll.as_view(),name='products'),
+    path('',index_page_view,name='products'),
 ]
--- product/views.py
+++ product/views.py
@@ -3,8 +3,18 @@ from django.views.generic import ListView
 # Create your views here.
 from .models import Products
 
-class ProductAll(ListView):
-    model = Products
-    template_name = 'list.html'
-    context_object_name = 'alls'
+# class ProductAll(ListView):
+#     model = Products
+#     template_name = 'list.html'
+#     context_object_name = 'alls'
 
+
+def index_page_view(request):
+    data = Products.objects.all()
+
+    context = {
+        'data': data,
+    }
+
+
+    return render(request,'index.html',context)
